1accabb4f366288c7a8da00450c5d54e32a47a6d
Marco <marcosiegman01@gmail.com>
2021-01-15 18:04:02 -0600

    add: notifications from Medium

--- src/views/Notifications/index.jsx
+++ src/views/Notifications/index.jsx
@@ -1,11 +1,19 @@
-import { ComposedLayout as Layout } from 'Components/Layout';
+import useMediumPublications from 'Hooks/useMediumPublications';
 
+import { ComposedLayout as Layout } from 'Components/Layout';
 import EmptyNotifications from './Components/EmptyNotifications';
+import NotificationsList from './Components/NotificationsList';
 
 function Notifications() {
+  const { publications } = useMediumPublications();
+
   return (
     <Layout title="Notifications">
-      <EmptyNotifications />
+      {publications.length === 0 ? (
+        <EmptyNotifications />
+      ) : (
+        <NotificationsList items={publications} />
+      )}
     </Layout>
   );
 }
